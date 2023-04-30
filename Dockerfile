efc17e4718c142ebbdfe28e8e7de6a53ca5318b9
Hayden Home <haydensoo@bluguard.com.my>
2023-05-01 01:17:05 +0800

    remove docker-compose

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ COPY config/config.json ./config/config.json
 RUN npm install
 COPY . /app
 EXPOSE 3000
-RUN npm run migrate
 CMD ["npm", "start"]
--- models/index.js
+++ models/index.js
@@ -11,14 +11,14 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config,{
-    host: 'localhost',
+    host: 'mysql',
     dialect: 'mysql',
     operatorsAliases: 'false',
     logging: false
   });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config,{
-    host: 'localhost',
+    host: 'mysql',
     dialect: 'mysql',
     operatorsAliases: 'false',
     logging: false
