a2d5fbddf77e1306f03eb9011a03f99eea1f1774
Hayden Soo <haydensoo96@gmail.com>
2023-05-07 16:30:51 +0800

    add cors

--- bin/api.js
+++ bin/api.js
@@ -1,15 +1,23 @@
 var app = require('../app')
 var http = require('http')
+const cors = require('cors')
 
 
 const port = 3000
 console.log('Starting API Server at Port', port)
 
 app.set('port', port)
+app.use(cors())
 
 var server = http.createServer(app)
 
 
 server.listen(port)
+
+app.use(cors({
+    origin: 'http://etiqa-fe.s3-website-ap-southeast-1.amazonaws.com',
+    methods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
 
